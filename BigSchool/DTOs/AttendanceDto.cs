3c8be64e363caf69a1743e32e71ed91e38b0a30e
minh8250vn <minh8250vn@gmail.com>
2020-06-26 13:17:05 +0700

    Add project files.

---
+++ BigSchool/DTOs/AttendanceDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Controllers
+{
+    public class AttendanceDto
+    {
+        public int CourseId { get; set; }
+    }
+}
